dcf447155c794fbc18c40046f15025feb7e62b66
Leonid-droid <lealro2011@gmail.com>
2020-01-05 22:32:22 +0300

    Выполнение 1-го задания

---
+++ task_01/script.js
@@ -0,0 +1,21 @@
+let money = +prompt('Ваш бюджет на месяц?');
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let expensesData = prompt('Введите обязательную статью расходов в этом месяце');
+let moneyExpenses = +prompt('Во сколько обойдется?');
+
+let appData = {
+    moneyData: money,
+    timeData: time,
+    expenses: { // объект с обязательными расходами
+        [expensesData]: moneyExpenses // переменная вместо ключа в []
+    },
+    optionalExpenses: { // объект с необязательными расходами
+    },
+    incam: Array = [], // массив данных с доп. доходом
+    savings: false
+};
+
+let moneyDay = (money / 30) - (moneyExpenses / 30);
+alert(moneyDay);
+console.log(appData);
+console.log(moneyDay);
